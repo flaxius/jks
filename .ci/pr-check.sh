922f1b9875c7ea30db433a50228d0ee10174ab38
Flavius Lacatusu <flavius.lacatusu@gmail.com>
2020-01-31 18:50:14 +0100

    first commit

--- .ci/pr-check.sh
+++ .ci/pr-check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+set -x
 
 source ${BASE_DIR}/util/common.sh
+  if [ ! -d "$OPERATOR_REPO/tmp" ]; then mkdir -p "$OPERATOR_REPO/tmp" && chmod 777 "$OPERATOR_REPO/tmp"; fi
 installStartDocker
 install_required_packages
+setup_kvm_machine_driver
+minishift_installation
--- .ci/util/common.sh
+++ .ci/util/common.sh
@@ -23,3 +23,35 @@ install_required_packages() {
     sudo systemctl start libvirtd
     echo '[INFO]CICO: Required virtualization packages installed'
 }
+
+setup_kvm_machine_driver() {
+    echo "[INFO] Installing docker machine kvm drivers..."
+    sudo curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 -o /usr/bin/docker-machine-driver-kvm
+    sudo chmod +x /usr/bin/docker-machine-driver-kvm
+    sudo check_libvirtd=$(systemctl is-active libvirtd)
+    if [ $check_libvirtd != 'active' ]; then
+        virsh net-start default
+    fi
+}
+
+minishift_installation() {
+  MSFT_RELEASE="1.34.2"
+  echo "[INFO] Downloading Minishift binaries..."
+  sudo curl -s -S -L https://github.com/minishift/minishift/releases/download/v$MSFT_RELEASE/minishift-$MSFT_RELEASE-linux-amd64.tgz \
+    -o ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && tar -xvf ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/bin --strip-components=1
+  echo "[INFO] Sarting a new OC cluster."
+  minishift start --memory=4096 && eval $(minishift oc-env)
+  oc login -u system:admin
+  oc adm policy add-cluster-role-to-user cluster-admin developer && oc login -u developer -p developer
+}
+
+generate_self_signed_certs() {
+    IP_ADDRESS="172.17.0.1"
+    openssl req -x509 \
+                -newkey rsa:4096 \
+                -keyout key.pem \
+                -out cert.pem \
+                -days 365 \
+                -subj "/CN=*.${IP_ADDRESS}.nip.io" \
+                -nodes && cat cert.pem key.pem > ca.crt    
+}
