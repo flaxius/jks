7305d433c9a3a4e27cdb88001d277a32259eaeb2
Flavius Lacatusu <flavius.lacatusu@gmail.com>
2020-01-31 19:49:38 +0100

    a

--- .ci/util/common.sh
+++ .ci/util/common.sh
@@ -1,47 +1,59 @@
-#!/bin/bash
+#
+# Copyright (c) 2012-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
 
+set -e
 
 installStartDocker() {
   if [ -x "$(command -v docker)" ]; then
     echo "[INFO] Docker already installed"
   else
     echo "[INFO] Installing docker..."
-    yum install --assumeyes -d1 yum-utils device-mapper-persistent-data lvm2
-    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    yum install --assumeyes -d1 docker-ce
-    systemctl start docker
-    docker version
+    sudo yum install --assumeyes -d1 yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install --assumeyes -d1 docker-ce
+    sudo systemctl start docker
+    sudo docker version
   fi
 }
 
 install_required_packages() {
-    sudo apt -y install qemu-kvm libvirt-daemon libvirt-daemon-system
-    sudo apt -y install libvirt-bin qemu-kvm
-    sudo usermod -a -G libvirt $(whoami)
-    sudo curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-ubuntu16.04 -o /usr/local/bin/docker-machine-driver-kvm
-    sudo chmod +x /usr/local/bin/docker-machine-driver-kvm
-    sudo systemctl start libvirtd
-    echo '[INFO]CICO: Required virtualization packages installed'
+    # Install EPEL repo
+    # Get all the deps in
+    sudo yum -y install libvirt qemu-kvm
+  echo '[INFO]CICO: Required virtualization packages installed'
+}
+
+start_libvirt() {
+  sudo systemctl start libvirtd
 }
 
 setup_kvm_machine_driver() {
     echo "[INFO] Installing docker machine kvm drivers..."
     sudo curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 -o /usr/bin/docker-machine-driver-kvm
     sudo chmod +x /usr/bin/docker-machine-driver-kvm
+    check_libvirtd=$(systemctl is-active libvirtd)
+    if [ $check_libvirtd != 'active' ]; then
+        virsh net-start default
+    fi
 }
 
 minishift_installation() {
   MSFT_RELEASE="1.34.2"
-      check_libvirtd=$(systemctl is-active libvirtd)
-newgrp libvirt
   echo "[INFO] Downloading Minishift binaries..."
   sudo curl -s -S -L https://github.com/minishift/minishift/releases/download/v$MSFT_RELEASE/minishift-$MSFT_RELEASE-linux-amd64.tgz \
-    -o ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && sudo tar -xvf ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/local/bin --strip-components=1
+    -o ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && sudo tar -xvf ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/bin --strip-components=1
   echo "[INFO] Sarting a new OC cluster."
-  sudo minishift config set skip-check-kvm-driver true
-  sudo minishift start --memory=4096 && sudo eval $(minishift oc-env)
-  sudo oc login -u system:admin
-  sudo oc adm policy add-cluster-role-to-user cluster-admin developer && oc login -u developer -p developer
+  minishift start --memory=4096 && eval $(minishift oc-env)
+  oc login -u system:admin
+  oc adm policy add-cluster-role-to-user cluster-admin developer && oc login -u developer -p developer
 }
 
 generate_self_signed_certs() {
