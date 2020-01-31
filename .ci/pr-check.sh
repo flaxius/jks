865f66a6a606bed529518138bde2e13cb023c820
Flavius Lacatusu <flavius.lacatusu@gmail.com>
2020-01-31 18:54:29 +0100

    first commit

--- .ci/pr-check.sh
+++ .ci/pr-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 set -x
+  if [[ ${WORKSPACE} ]] && [[ -d ${WORKSPACE} ]]; then OPERATOR_REPO=${WORKSPACE}; else OPERATOR_REPO=$(dirname "$SCRIPTPATH"); fi
   if [ ! -d "$OPERATOR_REPO/tmp" ]; then mkdir -p "$OPERATOR_REPO/tmp" && chmod 777 "$OPERATOR_REPO/tmp"; fi
 
 source ${BASE_DIR}/util/common.sh
