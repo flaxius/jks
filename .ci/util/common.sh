85f1966406fd0e5253c09c4bdaa26565c1fed40b
Flavius Lacatusu <flavius.lacatusu@gmail.com>
2020-01-31 18:29:39 +0100

    first commit

--- .ci/util/common.sh
+++ .ci/util/common.sh
@@ -16,6 +16,8 @@ installStartDocker() {
 
 install_required_packages() {
      cat /etc/os-release
+     apk add --update nodejs npm
+     node -v
     apt install qemu-kvm libvirt-daemon libvirt-daemon-system
     apt install libvirt-bin qemu-kvm
     usermod -a -G libvirt $(whoami)
