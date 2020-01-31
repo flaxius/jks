192c2fe5e81fb8c40d9ecc1e027a7e2eba3ea01d
Flavius Lacatusu <flavius.lacatusu@gmail.com>
2020-01-31 18:52:33 +0100

    first commit

--- .ci/pr-check.sh
+++ .ci/pr-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 set -x
+  if [ ! -d "$OPERATOR_REPO/tmp" ]; then mkdir -p "$OPERATOR_REPO/tmp" && chmod 777 "$OPERATOR_REPO/tmp"; fi
 
 source ${BASE_DIR}/util/common.sh
-  if [ ! -d "$OPERATOR_REPO/tmp" ]; then mkdir -p "$OPERATOR_REPO/tmp" && chmod 777 "$OPERATOR_REPO/tmp"; fi
 installStartDocker
 install_required_packages
 setup_kvm_machine_driver
